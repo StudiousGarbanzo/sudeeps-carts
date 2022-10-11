12c2af5230299ef0dc33222ed424a2ee0bcf96a5
SD <harrypotter.starwars.shrish@gmail.com>
2022-10-11 22:46:25 +0530

    Clean it

--- src/main/java/io/github/boogiemonster1o1/sudeepscarts/client/gui/LoadingScreen.java
+++ src/main/java/io/github/boogiemonster1o1/sudeepscarts/client/gui/LoadingScreen.java
@@ -11,9 +11,6 @@ import org.lwjgl.glfw.GLFW;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.toast.SystemToast;
-import net.minecraft.client.toast.Toast;
-import net.minecraft.client.toast.ToastManager;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
 public class LoadingScreen extends CottonClientScreen {
--- src/main/java/io/github/boogiemonster1o1/sudeepscarts/client/gui/TrainResultsDescription.java
+++ src/main/java/io/github/boogiemonster1o1/sudeepscarts/client/gui/TrainResultsDescription.java
@@ -7,6 +7,7 @@ import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
 import io.github.cottonmc.cotton.gui.widget.WLabel;
 import io.github.cottonmc.cotton.gui.widget.WListPanel;
 import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
+import io.github.cottonmc.cotton.gui.widget.data.Insets;
 import io.github.studiousgarbanzo.sudeepscarts.object.TrainBetweenStations;
 import io.github.studiousgarbanzo.sudeepscarts.object.TrainsStatus;
 import org.lwjgl.glfw.GLFW;
@@ -15,6 +16,7 @@ import net.minecraft.text.Text;
 
 public class TrainResultsDescription extends FullscreenGuiDescription {
 	private final TrainsStatus status;
+	private int hMax = 20;
 
 	public TrainResultsDescription(TrainsStatus status) {
 		super();
@@ -25,18 +27,31 @@ public class TrainResultsDescription extends FullscreenGuiDescription {
 		root.add(new WLabel(Text.translatable("gui.sudeepscarts.train_results.title")), 20, 20);
 
 		WListPanel<TrainBetweenStations, WTrainElement> trainList = new WListPanel<>(Arrays.asList(status.trainsBetweenStations()), WTrainElement::new, (data, widget) -> widget.configure(data));
-		root.add(trainList, 20, 40, 400, 200);
+		trainList.layout();
+		trainList.setListItemHeight(hMax);
+		trainList.layout();
+		root.add(trainList, 20, 40, 400, hMax * 2 + 10);
 
 		root.validate(this);
 	}
 
-	public static class WTrainElement extends WPlainPanel {
+	public class WTrainElement extends WPlainPanel {
 		public WTrainElement() {
-			this.setBackgroundPainter(BackgroundPainter.createColorful(0x55000000));
+			this.setBackgroundPainter(BackgroundPainter.createColorful(0x33000000));
+			this.setInsets(Insets.ROOT_PANEL);
 		}
 
 		public void configure(TrainBetweenStations train) {
-			this.add(new WLabel(Text.literal(train.trainName() + " : " + train.fromStationName() + " - " + train.toStationName())), 0, 0);
+			this.add(new WLabel(Text.literal(train.trainName())), 0, 0);
+			Text departure = Text.of("Departure: " + train.fromStationName() + " - " + train.departureTime12hFormat() + " " + train.departureMeridiem() + ", " + train.fromDay());
+			this.add(new WLabel(departure), 0, 15);
+			Text arrival = Text.of("Arrival: " + train.toStationName() + " - " + train.arrivalTime12hFormat() + " " + train.arrivalMeridiem() + ", " + train.toDay());
+			this.add(new WLabel(arrival), 0, 25);
+			Text time = Text.of("Duration: " + train.durationHours() + ":" + train.durationMinutes());
+			this.add(new WLabel(time), 0, 35);
+			this.layout();
+			TrainResultsDescription.this.hMax = Math.max(TrainResultsDescription.this.hMax, this.getHeight());
+			this.setSize(this.getWidth(), TrainResultsDescription.this.hMax);
 		}
 	}
 
